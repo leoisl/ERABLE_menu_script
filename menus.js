f7a46b5364c8c0041fce7ff777dff24d85154473
leoisl <leandro.ishi.lima@gmail.com>
2018-08-24 14:36:39 +0200

    Update menus.js

--- menus.js
+++ menus.js
@@ -1,14 +1,14 @@
-jQuery(document).ready(function(){
-	linkSplit = jQuery("[hreflang=en]")[0].href.split("/")
-	page = linkSplit[linkSplit.indexOf("en")+1].toUpperCase()
-	navMenus = jQuery("[id^=nav_menu-]")
-	navMenus.each(function(index) {
-		menuTitle = jQuery(navMenus[index]).children('h3').html().toUpperCase()
-		if (page==menuTitle) {
-			jQuery(navMenus[index]).css('display', "")
-		}
-		else {
-			jQuery(navMenus[index]).css('display', "none")
-		}
-	});
-})
+function showMenu(menuName) {
+	jQuery(document).ready(function(){
+		navMenus = jQuery("[id^=nav_menu-]")
+		navMenus.each(function(index) {
+			menuTitle = jQuery(navMenus[index]).children('h3').html().toUpperCase()
+			if (menuName==menuTitle) {
+				jQuery(navMenus[index]).css('display', "")
+			}
+			else {
+				jQuery(navMenus[index]).css('display', "none")
+			}
+		});
+	})
+}
